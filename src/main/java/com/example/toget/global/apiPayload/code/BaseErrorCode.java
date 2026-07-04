9e2c9eca767e9e5a708abbc91eb2b622c3edcb66
오창준 <cjbormk11@gmail.com>
2026-07-05 07:29:22 +0900

    feat: 공통 응답 포맷(ApiResponse) 및 전역/비즈니스 에러 코드 구현

---
+++ src/main/java/com/example/toget/global/apiPayload/code/BaseErrorCode.java
@@ -0,0 +1,10 @@
+package com.example.toget.global.apiPayload.code;
+
+import org.springframework.http.HttpStatus;
+
+public interface BaseErrorCode {
+
+    HttpStatus getStatus();
+    String getCode();
+    String getMessage();
+}
