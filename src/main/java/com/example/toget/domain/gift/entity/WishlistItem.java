abaf857dd884310072b2378c4e346c9301af81f0
오창준 <cjbormk11@gmail.com>
2026-07-10 22:13:38 +0900

    feat: WishlistItem 엔티티 생성

---
+++ src/main/java/com/example/toget/domain/gift/entity/WishlistItem.java
@@ -0,0 +1,32 @@
+package com.example.toget.domain.gift.entity;
+
+import com.example.toget.global.entity.BaseEntity;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+public class WishlistItem extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "user_id", nullable = false)
+    private Long userId;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private Long price;
+
+    @Column(name = "purchase_url", nullable = true)
+    private String purchaseUrl;
+
+    @Column(name = "image_url", nullable = true)
+    private String imageUrl;
+}
