91dd35cb26c508f415663e92873b5bb4575681b9
오창준 <cjbormk11@gmail.com>
2026-07-10 22:28:55 +0900

    feat: FundingGift 엔티티 생성

---
+++ src/main/java/com/example/toget/domain/gift/entity/FundingGift.java
@@ -0,0 +1,32 @@
+package com.example.toget.domain.gift.entity;
+
+import com.example.toget.global.entity.BaseEntity;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+public class FundingGift extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "funding_id", nullable = false)
+    private Long fundingId;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private Long price;
+
+    @Column(name = "purchase_url")
+    private String purchaseUrl;
+
+    @Column(name = "image_url")
+    private String imageUrl;
+}
