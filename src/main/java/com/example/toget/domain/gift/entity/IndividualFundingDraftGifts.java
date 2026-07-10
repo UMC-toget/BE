7654f3ff4891f8824766bbafe6391f28a8fc431b
오창준 <cjbormk11@gmail.com>
2026-07-10 22:33:10 +0900

    feat: IndividualFundingDraftGifts 엔티티 생성

---
+++ src/main/java/com/example/toget/domain/gift/entity/IndividualFundingDraftGifts.java
@@ -0,0 +1,31 @@
+package com.example.toget.domain.gift.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+public class IndividualFundingDraftGifts {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "my_draft_id", nullable = false)
+    private Long myDraftId;
+
+    @Column
+    private String name;
+
+    @Column
+    private Long price;
+
+    @Column(name = "purchase_url")
+    private String purchaseUrl;
+
+    @Column(name = "image_url")
+    private String imageUrl;
+}
