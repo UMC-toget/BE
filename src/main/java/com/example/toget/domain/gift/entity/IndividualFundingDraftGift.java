90437c58bb1c71525905c414d4261aaf2b5a65c2
오창준 <cjbormk11@gmail.com>
2026-07-11 16:28:24 +0900

    refactor: IndividualFundingDraftGift 엔티티 이름 수정

--- src/main/java/com/example/toget/domain/gift/entity/IndividualFundingDraftGift.java
+++ src/main/java/com/example/toget/domain/gift/entity/IndividualFundingDraftGift.java
@@ -9,7 +9,7 @@ import lombok.*;
 @Builder
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor
-public class IndividualFundingDraftGifts {
+public class IndividualFundingDraftGift {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
