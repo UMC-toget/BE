1aef131971c7318f578eb1b3c6b522f927b345a9
오창준 <cjbormk11@gmail.com>
2026-07-20 12:31:16 +0900

    fix: Swagger 문서에서 @LoginUserId 파라미터 제외 설정 추가

--- src/main/java/com/example/toget/global/config/SwaggerConfig.java
+++ src/main/java/com/example/toget/global/config/SwaggerConfig.java
@@ -1,16 +1,22 @@
 package com.example.toget.global.config;
 
+import com.example.toget.domain.user.controller.LoginUserId;
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
 import io.swagger.v3.oas.models.servers.Server;
+import org.springdoc.core.utils.SpringDocUtils;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class SwaggerConfig {
+
+    static {
+        SpringDocUtils.getConfig().addAnnotationsToIgnore(LoginUserId.class);
+    }
 
     @Bean
     public OpenAPI swagger() {
